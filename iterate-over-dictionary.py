0ba8e6b120f0500ae12bff4a9b77d72fc98d5fb7
Dogac Akyıldız <ndakyildiz@gmail.com>
2019-03-19 16:52:27 +0300

    Create iterate-over-dictionary.py

---
+++ iterate-over-dictionary.py
@@ -0,0 +1,11 @@
+d = {'x': 1, 'y': 2, 'z': 3} 
+
+for key in d:
+    print key, 'corresponds to', d[key]
+    
+# this loops through keys in the dictionary
+
+for key, value in d.items():
+    print(key, value)
+    
+# this loops through all items in a dict
